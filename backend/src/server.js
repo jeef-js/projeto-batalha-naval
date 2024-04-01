6d016a3f65220f53e2c268f0065ab82e6728801a
Jefferson Thiago Gomes de Araújo <v001250@intrajus.tjrn>
2024-04-01 16:15:49 -0300

    adicionando id de clients

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,23 @@
 const WebSocketServer = require('ws').Server;
+const uuid = require('uuid');
 
 const PORT = 8081;
 
 const wsServer = new WebSocketServer({ 'port': PORT });
 
+const clients = new Map();
+
 wsServer.on('connection', (client) => {
-    console.log('Um cliente se conectou ao servidor');
+    clients.set(uuid.v4(), { connection: client });
+
+    console.log('Um cliente se conectou ao servidor e recebeu o id: ' +  clients[client].id);
+
+    client.on('message', (message) => {
+        const payload = {
+            user: clients[client].id,
+            message: message
+        };
+
+        wsServer.send(JSON.stringify(payload));
+    });
 });
